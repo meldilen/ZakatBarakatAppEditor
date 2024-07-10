41ce1334d4c0b9fe8a1da99734fd6b9f2e6b756c
meldilen <123398352+meldilen@users.noreply.github.com>
2024-07-10 21:13:14 +0300

    homepage design

--- lib/UI/home_page.dart
+++ lib/UI/home_page.dart
@@ -6,103 +6,203 @@ class HomePage extends ConsumerStatefulWidget {
 
   @override
   ConsumerState<HomePage> createState() => _HomePageState();
-  
 }
 
 class _HomePageState extends ConsumerState<HomePage> {
-
+  final List<Map<String, dynamic>> buttonData = [
+    {'title': 'Articles', 'route': '/article_list'},
+    {'title': 'Questions\n&\nAnswers', 'route': '/question_list'},
+    {'title': 'Charity Funds', 'route': '/fund_list'},
+    {'title': 'Organizations', 'route': '/organization_list'},
+    {'title': 'News', 'route': '/news_list'},
+    {'title': 'User Requests', 'route': '/request_list'},
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.deepPurple,
-        title: const Text(
-          'Home Page', 
-          style: TextStyle(
-            fontSize: 22, 
-            fontWeight: FontWeight.bold, 
-            color: Colors.white)
-        ),
-        centerTitle: true,
-        elevation: 2,
-      ),
-      body: SafeArea(
-        top: true,
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children:  [
-            Expanded(
-              child: Align(
-                alignment: AlignmentDirectional(0, 0),
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/article_list');
-                  },
-                  child: const Text('Articles'),
-                )
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 88, 96, 85),
+      // appBar: AppBar(
+      //   backgroundColor: Colors.deepPurple,
+      //   title: const Text(
+      //     'Home Page',
+      //     style: TextStyle(
+      //       fontSize: 22,
+      //       fontWeight: FontWeight.bold,
+      //       color: Colors.white)
+      //   ),
+      //   centerTitle: true,
+      //   elevation: 2,
+      // ),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            backgroundColor: Color.fromARGB(255, 29, 43, 54),
+            expandedHeight: 300,
+            collapsedHeight: 85,
+            floating: false,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              background: Container(
+                color: Color.fromARGB(255, 29, 43, 54),
+              ),
+              title: Text(
+                'EDITOR',
+                style: TextStyle(
+                    fontFamily: 'Times',
+                    color: Colors.white,
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold),
               ),
             ),
-            Expanded(
-              child: Align(
-                alignment: AlignmentDirectional(0, 0),
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/question_list');
-                  },
-                  child: const Text('Q&As'),
-                )
-              ),
-            ),
-            Expanded(
-              child: Align(
-                alignment: AlignmentDirectional(0, 0),
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/fund_list');
-                  },
-                  child: const Text('Funds'),
-                )
-              ),
-            ),
-            Expanded(
-              child: Align(
-                alignment: AlignmentDirectional(0, 0),
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/news_list');
-                  },
-                  child: const Text('News'),
-                )
-              ),
-            ),
-            Expanded(
-              child: Align(
-                alignment: AlignmentDirectional(0, 0),
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/organization_list');
-                  },
-                  child: const Text('Organizations'),
-                )
-              ),
-            ),
-            Expanded(
-              child: Align(
-                alignment: AlignmentDirectional(0, 0),
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/request_list');
-                  },
-                  child: const Text('Requests'),
-                )
-              ),
-            ),
-          ],
-        ),
-      ),
+          ),
 
+          SliverPadding(
+            padding: const EdgeInsets.only(
+                top: 50, left: 350, right: 350, bottom: 50),
+            sliver: SliverGrid(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                crossAxisSpacing: 30,
+                mainAxisSpacing: 30,
+              ),
+              delegate: SliverChildBuilderDelegate(
+                (context, index) {
+                  return GestureDetector(
+                    onTap: () {
+                      Navigator.pushNamed(context, buttonData[index]['route']);
+                    },
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: Container(
+                        height: 100,
+                        color: const Color.fromARGB(255, 209, 217, 219),
+                        alignment: Alignment.center,
+                        padding: EdgeInsets.all(15),
+                        child: Text(
+                          buttonData[index]['title'],
+                          textAlign: TextAlign.center,
+                          style: const TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+                childCount: 6,
+              ),
+            ),
+          ),
+          // SliverToBoxAdapter(
+          //   child: Padding(
+          //     padding: const EdgeInsets.all(20.0),
+          //     child: ClipRRect(
+          //       borderRadius: BorderRadius.circular(20),
+          //       child: Container(
+          //         height: 400,
+          //         color: Color.fromARGB(255, 209, 217, 219),
+          //       ),
+          //     ),
+          //   ),
+          // ),
+          // SliverToBoxAdapter(
+          //   child: Padding(
+          //     padding: const EdgeInsets.all(20.0),
+          //     child: ClipRRect(
+          //       borderRadius: BorderRadius.circular(20),
+          //       child: Container(
+          //         height: 400,
+          //         color: Color.fromARGB(255, 209, 217, 219),
+          //       ),
+          //     ),
+          //   ),
+          // ),
+          // SliverToBoxAdapter(
+          //   child: Padding(
+          //     padding: const EdgeInsets.all(20.0),
+          //     child: ClipRRect(
+          //       borderRadius: BorderRadius.circular(20),
+          //       child: Container(
+          //         height: 400,
+          //         color: Color.fromARGB(255, 209, 217, 219),
+          //       ),
+          //     ),
+          //   ),
+          // )
+        ],
+      ),
     );
+    //   SafeArea(
+    //     top: true,
+    //     child: Column(
+    //       mainAxisSize: MainAxisSize.max,
+    //       mainAxisAlignment: MainAxisAlignment.start,
+    //       children: [
+    //         Expanded(
+    //           child: Align(
+    //               alignment: AlignmentDirectional(0, 0),
+    //               child: ElevatedButton(
+    //                 onPressed: () {
+    //                   Navigator.pushNamed(context, '/article_list');
+    //                 },
+    //                 child: const Text('Articles'),
+    //               )),
+    //         ),
+    //         Expanded(
+    //           child: Align(
+    //               alignment: AlignmentDirectional(0, 0),
+    //               child: ElevatedButton(
+    //                 onPressed: () {
+    //                   Navigator.pushNamed(context, '/question_list');
+    //                 },
+    //                 child: const Text('Q&As'),
+    //               )),
+    //         ),
+    //         Expanded(
+    //           child: Align(
+    //               alignment: AlignmentDirectional(0, 0),
+    //               child: ElevatedButton(
+    //                 onPressed: () {
+    //                   Navigator.pushNamed(context, '/fund_list');
+    //                 },
+    //                 child: const Text('Funds'),
+    //               )),
+    //         ),
+    //         Expanded(
+    //           child: Align(
+    //               alignment: AlignmentDirectional(0, 0),
+    //               child: ElevatedButton(
+    //                 onPressed: () {
+    //                   Navigator.pushNamed(context, '/news_list');
+    //                 },
+    //                 child: const Text('News'),
+    //               )),
+    //         ),
+    //         Expanded(
+    //           child: Align(
+    //               alignment: AlignmentDirectional(0, 0),
+    //               child: ElevatedButton(
+    //                 onPressed: () {
+    //                   Navigator.pushNamed(context, '/organization_list');
+    //                 },
+    //                 child: const Text('Organizations'),
+    //               )),
+    //         ),
+    //         Expanded(
+    //           child: Align(
+    //               alignment: AlignmentDirectional(0, 0),
+    //               child: ElevatedButton(
+    //                 onPressed: () {
+    //                   Navigator.pushNamed(context, '/request_list');
+    //                 },
+    //                 child: const Text('Requests'),
+    //               )),
+    //         ),
+    //       ],
+    //     ),
+    //   ),
+    // );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -28,56 +28,73 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-  runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => ArticleListViewModel()),
-        ChangeNotifierProvider(create: (_) => QuestionListViewModel()),
-        ChangeNotifierProvider(create: (_) => FundListViewModel()),
-        ChangeNotifierProvider(create: (_) => NewsListViewModel()),
-        ChangeNotifierProvider(create: (_) => OrganisationListViewModel()),
-        ChangeNotifierProvider(create: (_) => RequestListViewModel()),
-      ],
-      child: const MyApp(),)
-  );
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider(create: (_) => ArticleListViewModel()),
+      ChangeNotifierProvider(create: (_) => QuestionListViewModel()),
+      ChangeNotifierProvider(create: (_) => FundListViewModel()),
+      ChangeNotifierProvider(create: (_) => NewsListViewModel()),
+      ChangeNotifierProvider(create: (_) => OrganisationListViewModel()),
+      ChangeNotifierProvider(create: (_) => RequestListViewModel()),
+    ],
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.deepPurple,),
+      theme: ThemeData(primarySwatch: Colors.deepPurple, fontFamily: 'Times'),
       home: const HomePage(),
       routes: {
         '/home': (context) => const HomePage(),
         '/article_list': (context) => const ArticleListPage(),
         '/article_creation': (context) => CreateArticlePage(),
-        '/article_editing': (context) => EditArticlePage( article: ModalRoute.of(context)!.settings.arguments as ArticleViewModel,),
+        '/article_editing': (context) => EditArticlePage(
+              article: ModalRoute.of(context)!.settings.arguments
+                  as ArticleViewModel,
+            ),
         '/question_list': (context) => const QuestionListPage(),
         '/question_creation': (context) => CreateQuestionPage(),
-        '/question_editing': (context) => EditQuestionPage( question: ModalRoute.of(context)!.settings.arguments as QuestionViewModel,),
+        '/question_editing': (context) => EditQuestionPage(
+              question: ModalRoute.of(context)!.settings.arguments
+                  as QuestionViewModel,
+            ),
         '/fund_list': (context) => const FundListPage(),
         '/fund_creation': (context) => CreateFundPage(),
-        '/fund_editing': (context) => EditFundPage( fund: ModalRoute.of(context)!.settings.arguments as FundViewModel,),
+        '/fund_editing': (context) => EditFundPage(
+              fund: ModalRoute.of(context)!.settings.arguments as FundViewModel,
+            ),
         '/news_list': (context) => const NewsListPage(),
         '/news_creation': (context) => CreateNewsArticlePage(),
-        '/news_editing': (context) => EditNewsArticlePage( newsArticle: ModalRoute.of(context)!.settings.arguments as NewsViewModel,),
+        '/news_editing': (context) => EditNewsArticlePage(
+              newsArticle:
+                  ModalRoute.of(context)!.settings.arguments as NewsViewModel,
+            ),
         '/organization_list': (context) => const OrganizationsListPage(),
         '/organization_creation': (context) => CreateOrganizationPage(),
-        '/organization_editing': (context) => EditOrganizationPage( organization: ModalRoute.of(context)!.settings.arguments as OrganizationViewModel,),
+        '/organization_editing': (context) => EditOrganizationPage(
+              organization: ModalRoute.of(context)!.settings.arguments
+                  as OrganizationViewModel,
+            ),
         '/request_list': (context) => const RequestListPage(),
-        '/request_answering': (context) =>  RequestAnswerTypePage(request: ModalRoute.of(context)!.settings.arguments as RequestViewModel,),
-        '/request_answer_as_question': (context) =>  AnswerAsQuestionPage(request: ModalRoute.of(context)!.settings.arguments as RequestViewModel,),
-        '/request_answer_as_article': (context) =>  AnswerAsArticlePage(request: ModalRoute.of(context)!.settings.arguments as RequestViewModel,),
+        '/request_answering': (context) => RequestAnswerTypePage(
+              request: ModalRoute.of(context)!.settings.arguments
+                  as RequestViewModel,
+            ),
+        '/request_answer_as_question': (context) => AnswerAsQuestionPage(
+              request: ModalRoute.of(context)!.settings.arguments
+                  as RequestViewModel,
+            ),
+        '/request_answer_as_article': (context) => AnswerAsArticlePage(
+              request: ModalRoute.of(context)!.settings.arguments
+                  as RequestViewModel,
+            ),
       },
     );
   }
-
-
-
 }
-
