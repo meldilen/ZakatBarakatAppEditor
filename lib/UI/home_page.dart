593497e6e282a76e5abd8dce95d0acb922d44863
meldilen <123398352+meldilen@users.noreply.github.com>
2024-07-10 21:43:16 +0300

    change padding for sliver

--- lib/UI/home_page.dart
+++ lib/UI/home_page.dart
@@ -22,18 +22,6 @@ class _HomePageState extends ConsumerState<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 88, 96, 85),
-      // appBar: AppBar(
-      //   backgroundColor: Colors.deepPurple,
-      //   title: const Text(
-      //     'Home Page',
-      //     style: TextStyle(
-      //       fontSize: 22,
-      //       fontWeight: FontWeight.bold,
-      //       color: Colors.white)
-      //   ),
-      //   centerTitle: true,
-      //   elevation: 2,
-      // ),
       body: CustomScrollView(
         slivers: [
           SliverAppBar(
@@ -56,10 +44,11 @@ class _HomePageState extends ConsumerState<HomePage> {
               ),
             ),
           ),
-
           SliverPadding(
-            padding: const EdgeInsets.only(
-                top: 50, left: 350, right: 350, bottom: 50),
+            padding: EdgeInsets.symmetric(
+              horizontal: 250,
+              vertical: 50,
+            ),
             sliver: SliverGrid(
               gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                 crossAxisCount: 3,
@@ -95,114 +84,8 @@ class _HomePageState extends ConsumerState<HomePage> {
               ),
             ),
           ),
-          // SliverToBoxAdapter(
-          //   child: Padding(
-          //     padding: const EdgeInsets.all(20.0),
-          //     child: ClipRRect(
-          //       borderRadius: BorderRadius.circular(20),
-          //       child: Container(
-          //         height: 400,
-          //         color: Color.fromARGB(255, 209, 217, 219),
-          //       ),
-          //     ),
-          //   ),
-          // ),
-          // SliverToBoxAdapter(
-          //   child: Padding(
-          //     padding: const EdgeInsets.all(20.0),
-          //     child: ClipRRect(
-          //       borderRadius: BorderRadius.circular(20),
-          //       child: Container(
-          //         height: 400,
-          //         color: Color.fromARGB(255, 209, 217, 219),
-          //       ),
-          //     ),
-          //   ),
-          // ),
-          // SliverToBoxAdapter(
-          //   child: Padding(
-          //     padding: const EdgeInsets.all(20.0),
-          //     child: ClipRRect(
-          //       borderRadius: BorderRadius.circular(20),
-          //       child: Container(
-          //         height: 400,
-          //         color: Color.fromARGB(255, 209, 217, 219),
-          //       ),
-          //     ),
-          //   ),
-          // )
         ],
       ),
     );
-    //   SafeArea(
-    //     top: true,
-    //     child: Column(
-    //       mainAxisSize: MainAxisSize.max,
-    //       mainAxisAlignment: MainAxisAlignment.start,
-    //       children: [
-    //         Expanded(
-    //           child: Align(
-    //               alignment: AlignmentDirectional(0, 0),
-    //               child: ElevatedButton(
-    //                 onPressed: () {
-    //                   Navigator.pushNamed(context, '/article_list');
-    //                 },
-    //                 child: const Text('Articles'),
-    //               )),
-    //         ),
-    //         Expanded(
-    //           child: Align(
-    //               alignment: AlignmentDirectional(0, 0),
-    //               child: ElevatedButton(
-    //                 onPressed: () {
-    //                   Navigator.pushNamed(context, '/question_list');
-    //                 },
-    //                 child: const Text('Q&As'),
-    //               )),
-    //         ),
-    //         Expanded(
-    //           child: Align(
-    //               alignment: AlignmentDirectional(0, 0),
-    //               child: ElevatedButton(
-    //                 onPressed: () {
-    //                   Navigator.pushNamed(context, '/fund_list');
-    //                 },
-    //                 child: const Text('Funds'),
-    //               )),
-    //         ),
-    //         Expanded(
-    //           child: Align(
-    //               alignment: AlignmentDirectional(0, 0),
-    //               child: ElevatedButton(
-    //                 onPressed: () {
-    //                   Navigator.pushNamed(context, '/news_list');
-    //                 },
-    //                 child: const Text('News'),
-    //               )),
-    //         ),
-    //         Expanded(
-    //           child: Align(
-    //               alignment: AlignmentDirectional(0, 0),
-    //               child: ElevatedButton(
-    //                 onPressed: () {
-    //                   Navigator.pushNamed(context, '/organization_list');
-    //                 },
-    //                 child: const Text('Organizations'),
-    //               )),
-    //         ),
-    //         Expanded(
-    //           child: Align(
-    //               alignment: AlignmentDirectional(0, 0),
-    //               child: ElevatedButton(
-    //                 onPressed: () {
-    //                   Navigator.pushNamed(context, '/request_list');
-    //                 },
-    //                 child: const Text('Requests'),
-    //               )),
-    //         ),
-    //       ],
-    //     ),
-    //   ),
-    // );
   }
 }
