dc1912b8052489ddb4f71e1812b068ae08ca1cd1
meldilen <123398352+meldilen@users.noreply.github.com>
2024-07-12 03:23:09 +0300

    questions design done

--- lib/UI/QAs/Pages/question_list_page.dart
+++ lib/UI/QAs/Pages/question_list_page.dart
@@ -11,8 +11,6 @@ class QuestionListPage extends StatefulWidget {
 }
 
 class _QuestionListPageState extends State<QuestionListPage> {
-
-
   @override
   void initState() {
     super.initState();
@@ -20,51 +18,94 @@ class _QuestionListPageState extends State<QuestionListPage> {
   }
 
   Widget _buildUI(List<QuestionViewModel> questions) {
-    if(questions.isEmpty){
-    return const Center(child: Text('No questions found'));
-  }else{
-    return QuestionList(questions: questions);
-  }
-  }
-
-
-  Widget _buildButton(){
-    return FloatingActionButton(
-        backgroundColor: Color.fromARGB(255, 206, 72, 10),
-        onPressed: () {
-          Navigator.pushNamed(context, '/question_creation');
-        },
-        child: const Icon(Icons.add),
-      );
+    if (questions.isEmpty) {
+      return const Center(
+          child: Text('No questions found',
+              style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold)));
+    } else {
+      return QuestionList(questions: questions);
+    }
   }
 
-
-  AppBar _buildAppBar() {
-    return AppBar(
-        backgroundColor: Color.fromARGB(255, 206, 72, 10),
-        title: const Text(
-          'Questions', 
-          style: TextStyle(
-            fontSize: 22, 
-            fontWeight: FontWeight.bold, 
-            color: Colors.white)
+  Widget _buildButton() {
+    return ElevatedButton.icon(
+      onPressed: () {
+        Navigator.pushNamed(context, '/question_creation');
+      },
+      icon: Icon(Icons.add_circle, color: Colors.white),
+      label: Text(
+        'CREATE QUESTION',
+        style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+          fontSize: 20,
         ),
-        centerTitle: true,
-        elevation: 2,
-      );
+      ),
+      style: ElevatedButton.styleFrom(
+        padding: const EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
+        backgroundColor: Color.fromARGB(255, 29, 43, 54),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+      ),
+    );
   }
-
 
   @override
   Widget build(BuildContext context) {
     var questions = context.watch<QuestionListViewModel>().questions;
 
     return Scaffold(
-      appBar: _buildAppBar(),
-      body: _buildUI(questions),
-      floatingActionButton: _buildButton(),
+      backgroundColor: Color.fromARGB(255, 88, 96, 85),
+      body: NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return <Widget>[
+            SliverAppBar(
+              backgroundColor: Color.fromARGB(255, 29, 43, 54),
+              expandedHeight: 200,
+              collapsedHeight: 85,
+              floating: false,
+              pinned: true,
+              leading: IconButton(
+                padding: EdgeInsets.only(left: 10, top: 20),
+                icon: const Icon(Icons.arrow_back_ios_new_outlined,
+                    color: Colors.white, size: 40),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+              flexibleSpace: FlexibleSpaceBar(
+                background: Container(
+                  color: Color.fromARGB(255, 29, 43, 54),
+                ),
+                titlePadding:
+                    const EdgeInsetsDirectional.only(start: 0.0, bottom: 20.0),
+                title: Text(
+                  'QUESTIONS & ANSWERS',
+                  textAlign: TextAlign.start,
+                  style: TextStyle(
+                      fontFamily: 'Times',
+                      color: Colors.white,
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold),
+                ),
+                centerTitle: true,
+              ),
+            ),
+          ];
+        },
+        body: _buildUI(questions),
+      ),
+      floatingActionButton: Positioned(
+        bottom: 40,
+        child: Container(
+          alignment: Alignment.bottomCenter,
+          child: _buildButton(),
+        ),
+      ),
     );
   }
-  
-  
 }
