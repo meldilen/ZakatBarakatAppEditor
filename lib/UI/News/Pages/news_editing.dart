fd9cbe5e876136958571b90348281eabce20fdc3
meldilen <123398352+meldilen@users.noreply.github.com>
2024-07-13 02:52:34 +0300

    news design finished

--- lib/UI/News/Pages/news_editing.dart
+++ lib/UI/News/Pages/news_editing.dart
@@ -20,13 +20,19 @@ class _EditNewsArticlePageState extends State<EditNewsArticlePage> {
   late final TextEditingController _imageLinkController;
   late final quill.QuillController _quillController;
 
+  final _formKey = GlobalKey<FormState>();
+
   @override
   void initState() {
     super.initState();
     _nameController = TextEditingController(text: widget.newsArticle.name);
-    _sourceLinkController = TextEditingController(text: widget.newsArticle.sourceLink);
-    _imageLinkController = TextEditingController(text: widget.newsArticle.imageLink);
-    _tagControllers = widget.newsArticle.tags.map((tag) => TextEditingController(text: tag)).toList();
+    _sourceLinkController =
+        TextEditingController(text: widget.newsArticle.sourceLink);
+    _imageLinkController =
+        TextEditingController(text: widget.newsArticle.imageLink);
+    _tagControllers = widget.newsArticle.tags
+        .map((tag) => TextEditingController(text: tag))
+        .toList();
 
     final doc = quill.Document();
     doc.insert(0, widget.newsArticle.body);
@@ -65,12 +71,13 @@ class _EditNewsArticlePageState extends State<EditNewsArticlePage> {
     final image_link = _imageLinkController.text;
     final source_link = _sourceLinkController.text;
 
-    try{
-      await context.read<NewsListViewModel>().updateNewsArticle(widget.newsArticle.id ,name, body, image_link, source_link, tags);
+    try {
+      await context.read<NewsListViewModel>().updateNewsArticle(
+          widget.newsArticle.id, name, body, image_link, source_link, tags);
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(content: Text('News article updated successfully!')),
       );
-    }catch(e){
+    } catch (e) {
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(content: Text('Failed to update news article')),
       );
@@ -81,77 +88,288 @@ class _EditNewsArticlePageState extends State<EditNewsArticlePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Update News Article'),
-      ),
-      body: Column(
-            children: [
-              TextField(
-                controller: _nameController,
-                decoration: const InputDecoration(
-                  labelText: 'Name',
-                  border: OutlineInputBorder(),
-                ),
+      backgroundColor: Color.fromARGB(255, 88, 96, 85),
+      body: NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return <Widget>[
+            SliverAppBar(
+              backgroundColor: Color.fromARGB(255, 29, 43, 54),
+              expandedHeight: 200,
+              collapsedHeight: 85,
+              floating: false,
+              pinned: true,
+              leading: IconButton(
+                padding: EdgeInsets.only(left: 10, top: 20),
+                icon: const Icon(Icons.arrow_back_ios_new_outlined,
+                    color: Colors.white, size: 40),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
               ),
-              TextField(
-                controller: _imageLinkController,
-                decoration: const InputDecoration(
-                  labelText: 'Image Link',
-                  border: OutlineInputBorder(),
+              flexibleSpace: FlexibleSpaceBar(
+                background: Container(
+                  color: Color.fromARGB(255, 29, 43, 54),
                 ),
-              ),
-              TextField(
-                controller: _sourceLinkController,
-                decoration: const InputDecoration(
-                  labelText: 'Source Link',
-                  border: OutlineInputBorder(),
+                titlePadding:
+                    const EdgeInsetsDirectional.only(start: 0.0, bottom: 20.0),
+                title: Text(
+                  'EDIT NEWS ARTICLE',
+                  textAlign: TextAlign.start,
+                  style: TextStyle(
+                      fontFamily: 'Times',
+                      color: Colors.white,
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold),
                 ),
+                centerTitle: true,
               ),
-              const SizedBox(height: 16),
-              ..._tagControllers.map((controller) {
-                final index = _tagControllers.indexOf(controller);
-                return Row(
-                  children: [
-                    Expanded(
-                      child: TextField(
-                        controller: controller,
-                        decoration: const InputDecoration(
-                          labelText: 'Tag',
-                          border: OutlineInputBorder(),
-                        ),
+            ),
+          ];
+        },
+        body: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                const SizedBox(height: 120),
+                SizedBox(
+                  width: 600,
+                  child: TextFormField(
+                    controller: _nameController,
+                    validator: (value) {
+                      if (value!.isEmpty) return 'Please enter some text';
+                      return null;
+                    },
+                    onChanged: (value) {},
+                    decoration: const InputDecoration(
+                      hintText: "Enter News title here",
+                      hintStyle: TextStyle(fontSize: 20),
+                      prefixIcon: Icon(Icons.newspaper_outlined),
+                      contentPadding:
+                          const EdgeInsets.symmetric(horizontal: 40.0),
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20.0)),
                       ),
                     ),
-                    IconButton(
-                      icon: const Icon(Icons.remove_circle),
-                      onPressed: () => _removeTagField(index),
+                    minLines: 1,
+                    maxLines: 5,
+                  ),
+                ),
+                const SizedBox(height: 16),
+                SizedBox(
+                  width: 600,
+                  child: TextFormField(
+                    controller: _imageLinkController,
+                    validator: (value) {
+                      if (value!.isEmpty) return 'Please enter some text';
+                      return null;
+                    },
+                    onChanged: (value) {},
+                    decoration: const InputDecoration(
+                      hintText: "Enter image link here",
+                      hintStyle: TextStyle(fontSize: 20),
+                      prefixIcon: Icon(Icons.link),
+                      contentPadding:
+                          const EdgeInsets.symmetric(horizontal: 40.0),
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                      ),
                     ),
-                  ],
-                );
-              }),
-              const SizedBox(height: 16),
-              ElevatedButton(
-                onPressed: _addTagField,
-                child: const Text('Add Tag'),
-              ),
-              const SizedBox(height: 16),
-              const SizedBox(height: 16),
-              Expanded(
-                child: QuillEditor.basic(
-                configurations: QuillEditorConfigurations(
-                  controller: _quillController,
+                    minLines: 1,
+                    maxLines: 5,
+                  ),
                 ),
+                const SizedBox(height: 16),
+                SizedBox(
+                  width: 600,
+                  child: TextFormField(
+                    controller: _sourceLinkController,
+                    validator: (value) {
+                      if (value!.isEmpty) return 'Please enter some text';
+                      return null;
+                    },
+                    onChanged: (value) {},
+                    decoration: const InputDecoration(
+                      hintText: "Enter source link here",
+                      hintStyle: TextStyle(fontSize: 20),
+                      prefixIcon: Icon(Icons.source_outlined),
+                      contentPadding:
+                          const EdgeInsets.symmetric(horizontal: 40.0),
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                      ),
+                    ),
+                    minLines: 1,
+                    maxLines: 5,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 16),
-              ElevatedButton(
-                onPressed: () => _updateNewsArticle(),
-                child: const Text('Save'),
-              ),
-              ElevatedButton(
-                onPressed: () => Navigator.pop(context), 
-                child: const Text('Cancel')),
-            ],
+                const SizedBox(height: 16),
+                ..._tagControllers.map((controller) {
+                  final index = _tagControllers.indexOf(controller);
+                  return Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SizedBox(
+                            width: 560,
+                            child: TextFormField(
+                              controller: controller,
+                              validator: (value) {
+                                if (value!.isEmpty)
+                                  return 'Please enter some text';
+                                return null;
+                              },
+                              onChanged: (value) {},
+                              decoration: const InputDecoration(
+                                hintText: "Enter News tag here",
+                                hintStyle: TextStyle(fontSize: 20),
+                                prefixIcon: Icon(Icons.queue),
+                                contentPadding: const EdgeInsets.symmetric(
+                                    horizontal: 40.0),
+                                filled: true,
+                                fillColor: Colors.white,
+                                border: OutlineInputBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(20.0)),
+                                ),
+                              ),
+                              minLines: 1,
+                              maxLines: 5,
+                            ),
+                          ),
+                          IconButton(
+                            icon: const Icon(
+                              Icons.remove_circle,
+                              color: Colors.white,
+                            ),
+                            onPressed: () => _removeTagField(index),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 5),
+                    ],
+                  );
+                }),
+                const SizedBox(height: 25),
+                ElevatedButton(
+                  onPressed: _addTagField,
+                  child: const Text(
+                    'Add Tag',
+                    style: TextStyle(fontSize: 20, color: Colors.white),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 40.0, vertical: 15.0),
+                    backgroundColor: Color.fromARGB(255, 29, 43, 54),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 25),
+                Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 42),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(8.0),
+                    color: Color.fromARGB(255, 209, 217, 219),
+                  ),
+                  height: 200,
+                  child: SingleChildScrollView(
+                    child: QuillEditor.basic(
+                      configurations: QuillEditorConfigurations(
+                        controller: _quillController,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 30),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  title: Text(
+                                      'Are you sure you want to update this News Article?'),
+                                  actions: [
+                                    TextButton(
+                                      onPressed: () {
+                                        _updateNewsArticle();
+                                        Navigator.of(context).pop();
+                                      },
+                                      child: const Text('Update',
+                                          style: TextStyle(
+                                              color: Color.fromARGB(
+                                                  255, 39, 72, 45),
+                                              fontSize: 15)),
+                                    ),
+                                    TextButton(
+                                      onPressed: () {
+                                        Navigator.of(context).pop();
+                                      },
+                                      child: const Text('Cancel',
+                                          style: TextStyle(
+                                              color: Color.fromARGB(
+                                                  255, 135, 7, 7),
+                                              fontSize: 15)),
+                                    ),
+                                  ],
+                                );
+                              },
+                            );
+                          }
+                        },
+                        child: const Text(
+                          'Update',
+                          style: TextStyle(fontSize: 20, color: Colors.white),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 56.0, vertical: 15.0),
+                          backgroundColor: Color.fromARGB(255, 29, 43, 54),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 40),
+                      ElevatedButton(
+                        onPressed: () => Navigator.pop(context),
+                        child: const Text(
+                          'Cancel',
+                          style: TextStyle(fontSize: 20, color: Colors.white),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 47.0, vertical: 15.0),
+                          backgroundColor: Color.fromARGB(255, 29, 43, 54),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
-        );
+        ),
+      ),
+    );
   }
 }
